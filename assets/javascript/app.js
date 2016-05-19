5b0a1459ba8c3b601711bb7ab1aff462ae9551c7
emilysf <emilysfrazin@gmail.com>
2016-05-18 20:16:26 -0400

    js working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,5 +29,45 @@ $('#clickButton').on('click', function(){
 	$('#time').val('');
 	$('#freq').val('');
 
+	//prevents moving to new page
 	return false;
+})
+
+//event adds data to the current train schedule chart
+trainData.on("child_added", function(childSnapshot, prevChildKey){
+
+	//store everything in a variable
+	var trainName = childSnapshot.val().name;
+	var trainDestination = childSnapshot.val().destination;
+	var trainTime = childSnapshot.val().time;
+	var trainFreq = childSnapshot.val().freq;
+
+
+
+	//first train time
+	var firstTimeConverted = moment(trainTime ,"HH:mm").subtract(1, "years");
+	console.log(firstTimeConverted);
+	
+	//current time
+	var currentTime = moment();
+	console.log("Current time: " + moment(currentTime).format("HH:mm"));
+
+	var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	console.log("Time difference: " + diffTime);
+
+	//time apart
+	var tRemainder = diffTime % trainFreq; 
+	console.log("Time remaining: " + tRemainder);
+
+	//minutes until next train 
+	var trainMin = trainFreq - tRemainder;
+	console.log("Minutes until train: " + trainMin);
+
+	//nextTrain
+	var nextTrain = moment().add(trainMin, "minutes");
+	var nextArrival = moment(nextTrain).format("hh:mm A");
+	
+	//adds to train chart
+	$('#traintable > tbody').append('<tr><td>' + trainName + '</td><td>' + trainDestination + '</td><td>' + trainFreq + '</td><td>' + nextArrival + '</td><td>' + trainMin + '</td></tr>');
+
 })
